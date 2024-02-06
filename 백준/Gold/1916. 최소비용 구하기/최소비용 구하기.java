c3c8df2d77a5e6b29c285b459c816d48fca28568
정현호 <103182268+Heiness@users.noreply.github.com>
2024-02-06 14:21:39 +0900

    [Gold V] Title: 최소비용 구하기, Time: 392 ms, Memory: 50280 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,94 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static class Node implements Comparable<Node> {
+		int v, cost;
 
-    static ArrayList<Edge>[] adjacencyList;
-    static int[] distance;
-    static boolean[] visited;
-    static PriorityQueue<Edge> queue = new PriorityQueue<>();
+		public Node(int v, int cost) {
+			this.v = v;
+			this.cost = cost;
+		}
 
+		@Override
+		public int compareTo(Node edge) {
+			if (this.cost > edge.cost) {
+				return 1;
+			}
+			return -1;
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	static boolean[] visited;
+	static int[] dist;
+	static List<Node>[] adj;
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        adjacencyList = new ArrayList[n + 1];
-        for (int i = 1; i < adjacencyList.length; i++) {
-            adjacencyList[i] = new ArrayList<>();
-        }
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
 
-        distance = new int[n + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
+		dist = new int[N];
+		visited = new boolean[N];
+		adj = new ArrayList[N];
+		for (int i = 0; i < N; i++) {
+			adj[i] = new ArrayList<>();
+			dist[i] = Integer.MAX_VALUE;
+		}
+		for (int i = 0; i < M; i++) { // 인접 노드 넣기
+			st = new StringTokenizer(br.readLine());
+			adj[Integer.parseInt(st.nextToken()) - 1]
+					.add(new Node(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken())));
+		}
 
-        visited = new boolean[n + 1];
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		dijkstra(start - 1, end - 1);
+		System.out.println(dist[end - 1]);
+	}
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+	static void dijkstra(int start, int end) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
 
-            adjacencyList[start].add(new Edge(end, weight));
-        }
+		while (!pq.isEmpty()) {
+			Node now = pq.poll(); // class 필드 호출은 상당히 느림
+			int nowV = now.v;
+			int nowC = now.cost;
+//			if(!visited[now.v]) visited[now.v]= true;
+			if (visited[nowV] && nowV == end)
+				return;
+			else if (visited[nowV])
+				continue;
+//			if(dist[end]<now.cost) continue;
+			visited[nowV] = true;
 
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-
-        dijkstra(start, end);
-
-        System.out.println(distance[end]);
-    }
-
-    private static void dijkstra(int start, int end) {
-        distance[start] = 0;
-        queue.add(new Edge(start, 0));
-
-        while (!queue.isEmpty()) {
-            Edge current = queue.poll();
-            int currentV = current.vertex;
-
-            if (visited[currentV] && currentV == end) {
-                return;
-            } else if (visited[currentV]) {
-                continue;
-            }
-
-            visited[currentV] = true;
-
-            for (Edge adjacency : adjacencyList[currentV]) {
-                if (distance[adjacency.vertex] > distance[currentV] + adjacency.weight) {
-                    distance[adjacency.vertex] = distance[currentV] + adjacency.weight;
-                    queue.add(new Edge(adjacency.vertex, distance[adjacency.vertex]));
-                }
-            }
-        }
-    }
+			for (Node next : adj[nowV]) {
+				if (!visited[next.v] && dist[next.v] > nowC + next.cost) {
+					dist[next.v] = nowC + next.cost;
+					pq.add(new Node(next.v, dist[next.v]));
+				}
+			}
+		}
+	}
 }
-
-class Edge implements Comparable<Edge> {
-        int vertex;
-        int weight;
-
-        public Edge(int vertex, int weight) {
-            this.vertex = vertex;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Edge edge) {
-            if (this.weight > edge.weight) {
-                return 1;
-            }
-            return -1;
-        }
-    }
