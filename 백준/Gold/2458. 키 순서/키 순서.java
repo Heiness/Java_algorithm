167d8f734f9067a470af0b8aa2197d9eb266833f
정현호 <103182268+Heiness@users.noreply.github.com>
2024-02-07 11:44:33 +0900

    [Gold IV] Title: 키 순서, Time: 776 ms, Memory: 35024 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -9,19 +9,19 @@ public class Main {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int[][] graph = new int[N][N];
+		boolean[][] graph = new boolean[N][N];
 		
 		for(int i=0;i<M;i++) { // 대입
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken())-1;
 			int b = Integer.parseInt(st.nextToken())-1;
-			graph[a][b]=1; // 단방향 그래프
+			graph[a][b]=true; // 단방향 그래프
 		}
 		
 		for(int k=0;k<N;k++) {
 			for(int i=0;i<N;i++) {
 				for(int j=0;j<N;j++) {
-					graph[i][j] = graph[i][j]>0 ||(graph[i][k]>0 && graph[k][j]>0) ? 1 : 0;
+					graph[i][j] = graph[i][j] ||(graph[i][k] && graph[k][j]);
 				}
 			}
 		} // 연결 가능 여부 check
@@ -29,7 +29,7 @@ public class Main {
 		for(int i=0;i<N;i++) {
 			cnt=0;
 			for(int j=0;j<N;j++) {
-				if(graph[i][j]>0||graph[j][i]>0) cnt++;
+				if(graph[i][j]||graph[j][i]) cnt++;
 			}
 			if(cnt==N-1) ans++;
 		}
