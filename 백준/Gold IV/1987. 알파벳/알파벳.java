9bd90e108c2ab1e69b6c9094a234b1fcc6f4dd22
정현호 <103182268+Heiness@users.noreply.github.com>
2024-02-16 09:17:51 +0900

    [Gold IV] Title: 알파벳, Time: 816 ms, Memory: 12296 KB -BaekjoonHub

--- 백준/Gold IV/1987. 알파벳/알파벳.java
+++ 백준/Gold IV/1987. 알파벳/알파벳.java
@@ -5,7 +5,8 @@ public class Main {
 	static int[] dy = {-1,1,0,0};
 	static int[] dx = {0,0,-1,1};
 	static int N,M,ans;
-	static char[][] arr;
+	static int[][] arr;
+	static boolean[] visited = new boolean[26];
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,21 +14,28 @@ public class Main {
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new char[N][M];
+		arr = new int[N][M];
 		
-		for(int i=0;i<N;i++) arr[i] = br.readLine().toCharArray();
+		for(int i=0;i<N;i++) {
+			String input = br.readLine();
+			for(int j=0;j<M;j++) arr[i][j] = input.charAt(j)-'A'; // A~Z를 0~26으로 관리
+		}
 		
-		dfs(arr[0][0]+"",1,0,0);
+		visited[arr[0][0]] = true;
+		dfs(1,0,0);
 		System.out.println(ans);
 		
 		
 	}
-	static void dfs(String visited, int length, int y, int x) {
+	static void dfs(int length, int y, int x) {
 		for(int i=0;i<4;i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(0<=ny&&ny<N&&0<=nx&&nx<M&&!visited.contains(arr[ny][nx]+"")) {
-				dfs(visited+arr[ny][nx], length+1, ny, nx);
+			if(0<=ny&&ny<N&&0<=nx&&nx<M&&!visited[arr[ny][nx]]) {
+				int alp = arr[ny][nx];
+				visited[alp] = true;
+				dfs(length+1, ny, nx);
+				visited[alp] = false;
 			}
 		}
 		
