378033c37d69dd9352fc01634bbd2f063637d654
정현호 <103182268+Heiness@users.noreply.github.com>
2024-02-21 10:47:29 +0900

    [Gold V] Title: 암호 만들기, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -5,8 +5,8 @@ public class Main {
 	static int L,C;
 	static char[] alphas;
 	static char[] answer;
-	static boolean[] visited;
 	static StringBuilder sb;
+	static String mo = "aeiou";
 	public static void main(String[] arg) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
@@ -14,7 +14,6 @@ public class Main {
 		
 		L = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		visited = new boolean[C];
 		
 		st = new StringTokenizer(br.readLine());
 		alphas = new char[C];
@@ -23,7 +22,7 @@ public class Main {
 		Arrays.sort(alphas);
 		comb(0,0,0,0);
 		System.out.println(sb);
-		
+		br.close();
 	}
 	
 	static void comb(int d, int start, int c1, int c2) {
@@ -31,15 +30,13 @@ public class Main {
 			if(c1>1 && c2>0) {
 				for(int i=0;i<L;i++) sb.append(answer[i]);
 				sb.append("\n");
-//				sb.append(String.valueOf(answer)).append("\n");
 			}
 			return;
 		}
 		
 		for(int i=start;i<C;i++) {
-			char tmp = alphas[i];
-			answer[d] = tmp;
-			if("aeiou".contains(tmp+"")) comb(d+1, i+1, c1, c2+1);
+			answer[d] = alphas[i];
+			if(answer[d]=='a'||answer[d]=='e'||answer[d]=='i'||answer[d]=='o'||answer[d]=='u') comb(d+1, i+1, c1, c2+1);
 			else comb(d+1, i+1, c1+1, c2);
 		}
 	}
