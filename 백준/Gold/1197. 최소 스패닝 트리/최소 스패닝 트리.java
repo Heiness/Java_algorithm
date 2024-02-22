564afe40be69fa0396071f949fe00e658cdddbd6
정현호 <103182268+Heiness@users.noreply.github.com>
2024-02-22 10:33:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 432 ms, Memory: 46608 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,99 +1,76 @@
-/**
- * 1. 에지리스트로 구현하고 유니온 파인트 배열 초기화
- * 2. 가중치 기준으로 오름차순 정렬
- * 3. 가중치가 가장 낮은 에지부터 연결 시도 
- */
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	static int V, E, result, edge;
+	static int v, e;
+	static int edgeCount, answer;
 	static int[] parent;
-	static boolean[] visited;
-	static Queue<Edge> q = new PriorityQueue<Edge>();
+	static PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-//	에지리스트
 	static class Edge implements Comparable<Edge> {
-		int start;
-		int end;
-		int value;
+		int a, b, weight;
 
-		public Edge(int start, int end, int value) {
-			this.start = start;
-			this.end = end;
-			this.value = value;
+		Edge(int a, int b, int weight) {
+			this.a = a;
+			this.b = b;
+			this.weight = weight;
 		}
 
-//		가중치 오름차순 
+		@Override
 		public int compareTo(Edge o) {
-
-			return this.value - o.value;
+			return this.weight - o.weight;
 		}
-
 	}
 
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		parent = new int[V + 1];
-
-//		1. 에지리스트로 구현하고 유니온 파인트 배열 초기화
-		for (int i = 0; i < V + 1; i++) {
+	static void makeSet() {
+		parent = new int[v];
+		for (int i = 0; i < v; i++) {
 			parent[i] = i;
 		}
+	}
 
-		for (int i = 0; i < E; i++) {
+	static int find(int i) {
+		if (i == parent[i]) return i;
+		return parent[i] = find(parent[i]);
+	}
+
+	static boolean union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a == b) return false; // 사이클이 발생하는 경우
+		if (a > b) parent[a] = b;
+		else parent[b] = a;
+		return true;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		v = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < e; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
-
-			// 2. 가중치 기준으로 오름차순 정렬 => comparable 사용하고 우선순위 큐에 담으면 자동 정렬
-			q.add(new Edge(start, end, value));
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			int weight = Integer.parseInt(st.nextToken());
+			pq.offer(new Edge(a, b, weight));
 		}
 
-//		MST를 만족하기 위해서는 에지 수가 최대 V - 1개
-		while (edge < V - 1) {
-			Edge cur = q.poll();
+		makeSet();
 
-//		    3. 가중치가 가장 낮은 에지부터 연결 시도 => 대표노드가 서로 다를 경우! , 만약 같다면 사이클이 형성됨 
-			if (find(cur.start) != find(cur.end)) {
-				union(cur.start, cur.end);
-				result += cur.value;
-				edge++;
+		// Kruskal 알고리즘을 이용하여 MST의 가중치를 구한다.
+		for (int i = 0; i < e; i++) {
+			Edge edge = pq.poll();
+			if (union(edge.a, edge.b)) {
+				answer += edge.weight;
+				if (++edgeCount == v - 1)
+					break;
 			}
 		}
 
-		sb.append(result);
-		System.out.println(sb);
-
+		System.out.println(answer);
 	}
-
-//	union 연산 
-	static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
-
-		if (x != y) {
-			parent[y] = x;
-		}
-	}
-
-//	find 연산 => 사이클 여부 확인
-	static int find(int x) {
-		if (x == parent[x])
-			return x;
-		return parent[x] = find(parent[x]);
-	}
-
 }
