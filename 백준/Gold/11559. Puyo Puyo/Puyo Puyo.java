7376c2edfcc54d0bbd608658b73dcc156c924bb5
정현호 <103182268+Heiness@users.noreply.github.com>
2024-02-29 10:26:55 +0900

    [Gold IV] Title: Puyo Puyo, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -14,20 +14,16 @@ class Point {
 
 public class Main {
 	static char[][] arr = new char[12][6];
-	static String input;
-	static int ans;
+	static int[] dy = { -1, 1, 0, 0 }, dx = { 0, 0, -1, 1 };
+	static int ans,chk;
 	static Queue<Point> q = new ArrayDeque<>();
-	static boolean[][] visited = new boolean[12][6];
-	static boolean[][] visitedCopy = new boolean[12][6];
-	static int[] dy = { -1, 1, 0, 0 };
-	static int[] dx = { 0, 0, -1, 1 };
+	static boolean[][] visited = new boolean[12][6], visitedCopy = new boolean[12][6];
 	static boolean g;
-	static int chk;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for (int i = 0; i < 12; i++) {
-			input = br.readLine();
+			String input = br.readLine();
 			for (int j = 0; j < input.length(); j++) {
 				arr[i][j] = input.charAt(j);
 			}
@@ -37,15 +33,7 @@ public class Main {
 			search(); // 끝나면 4개 이상이였던 좌표 visited 체크 되어있음.
 			boom(); // 터트리고 ans++
 			down(); // 내리기
-//			int c = 0;
-//			for (int i = 0; i < 12; i++) {
-//				for (int j = 0; j < 6; j++) {
-//					if (visited[i][j] == true)
-//						c++;
-//				}
-//			}
-			if (chk == 0)
-				g = true;
+			if (chk == 0) g = true;
 		}
 		System.out.println(ans);
 
@@ -56,9 +44,9 @@ public class Main {
 		for (int i = 0; i < 12; i++) {
 			for (int j = 0; j < 6; j++) {
 				if (arr[i][j] != '.' && visited[i][j] == false) { // 방문도 안했고 색깔이면 돌리기
-					cp(visitedCopy, visited);
+					copy(visitedCopy, visited);
 					if (bfs(new Point(i, j, arr[i][j])) < 4) { // 4이하면
-						cp(visited, visitedCopy); // 원상복귀
+						copy(visited, visitedCopy); // 원상복귀
 					}
 				}
 			}
@@ -76,7 +64,7 @@ public class Main {
 				int ny = cur.y + dy[i];
 				int nx = cur.x + dx[i];
 
-				if (0 <= nx && nx < 6 && 0 <= ny && ny < 12 && arr[ny][nx] == cur.c && visited[ny][nx] == false) { // 애초에
+				if (0 <= nx && nx < 6 && 0 <= ny && ny < 12 && arr[ny][nx] == cur.c && visited[ny][nx] == false) { // 같은 색깔이고 방문안한 경우만 이동
 					visited[ny][nx] = true;
 					Point np = new Point(ny, nx, arr[ny][nx]);
 					q.offer(np);
@@ -88,20 +76,18 @@ public class Main {
 	}
 
 	static void boom() {
-		int cnt = 0;
 		for (int i = 0; i < 12; i++) {
 			for (int j = 0; j < 6; j++) {
 				if (visited[i][j] == true) {
 					arr[i][j] = '.';
-					cnt++;
 					chk++;
 				}
 			}
 		}
-		if(cnt!=0) ans++;
+		if(chk!=0) ans++;
 	}
 
-	static void cp(boolean[][] a, boolean[][] b) {
+	static void copy(boolean[][] a, boolean[][] b) {
 		for (int i = 0; i < 12; i++) {
 			for (int j = 0; j < 6; j++) {
 				a[i][j] = b[i][j];
