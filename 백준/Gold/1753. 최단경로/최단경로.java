04f3723c302fc531d12c5577fa06c0347ac09c12
정현호 <103182268+Heiness@users.noreply.github.com>
2024-03-02 19:33:05 +0900

    [Gold IV] Title: 최단경로, Time: 856 ms, Memory: 121856 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,74 +1,61 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int V, E, K;
-    static ArrayList<Node>[] graph;
-    static int[] dijkstraTable;
-
-    static class Node {
-        int n;
-        int cost;
-
-        Node(int n, int cost) {
-            this.n = n;
-            this.cost = cost;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(br.readLine());
-
-        graph = new ArrayList[V + 1];
-        for (int i = 1; i < V + 1; i++) graph[i] = new ArrayList<>();
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new Node(v, w)); // 최단 거리로 갱신
-        }
-
-        dijkstra(K);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < V + 1; i++) {
-            if (i == K) {
-                sb.append(0).append("\n");
-            } else if (dijkstraTable[i] == Integer.MAX_VALUE) {
-                sb.append("INF").append("\n");
-            } else {
-                sb.append(dijkstraTable[i]).append("\n");
-            }
-        }
-        System.out.print(sb);
-    }
-
-    static void dijkstra(int s) {
-        dijkstraTable = new int[V + 1];
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
-            return o1.cost - o2.cost;
-        });
-
-        Arrays.fill(dijkstraTable, Integer.MAX_VALUE);
-        dijkstraTable[s] = 0;
-        pq.add(new Node(s, 0));
-
-        while (!pq.isEmpty()) {
-            Node curnode = pq.poll();
-
-            for (Node node : graph[curnode.n]) {
-                int newW = curnode.cost + node.cost;
-                int originW = dijkstraTable[node.n];
-                if (originW > newW) {
-                    dijkstraTable[node.n] = newW;
-                    pq.add(new Node(node.n, newW));
-                }
-            }
-        }
-    }
+	static int N,M;
+	static List<int[]>[] graph;
+	static PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->Integer.compare(o1[1], o2[1]));
+	static int[] dist;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		graph = new ArrayList[N+1];
+		dist = new int[N+1];
+		visited = new boolean[N+1];
+		for(int i=1;i<N+1;i++) {
+			graph[i] = new ArrayList<>();
+			dist[i] = Integer.MAX_VALUE;
+		}
+		
+		int S = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			graph[a].add(new int[] {b,c});
+		}
+		
+		dijk(S);
+		for(int i=1;i<N+1;i++) sb.append(dist[i]==Integer.MAX_VALUE?"INF":dist[i]).append("\n");
+		System.out.println(sb);
+	}
+	
+	static void dijk(int S) {
+		dist[S] = 0;
+		pq.offer(new int[] {S,0});
+		while(!pq.isEmpty()) {
+			int[] now = pq.poll();
+			int nowV = now[0];
+			int nowW = now[1];
+			if(visited[nowV]) continue;
+			visited[nowV] = true;
+			for(int[] next:graph[nowV]) {
+				int nextV = next[0];
+				int nextW = next[1];
+				int d = nowW+nextW;
+				if(!visited[nextV]&&dist[nextV]>d) {
+					pq.offer(new int[] {nextV, d});
+					dist[nextV] = d;
+				}
+			}
+		}
+	}
 }
