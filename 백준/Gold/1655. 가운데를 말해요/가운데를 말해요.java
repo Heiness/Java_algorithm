ab013ab6cb65550e45de662113f6b30c36a02d5c
정현호 <103182268+Heiness@users.noreply.github.com>
2024-03-08 17:25:00 +0900

    [Gold II] Title: 가운데를 말해요, Time: 364 ms, Memory: 31536 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,36 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq1 = new PriorityQueue<>((o1,o2)->-Integer.compare(o1, o2));
-		PriorityQueue<Integer> pq2 = new PriorityQueue<>();
-		int l1=0,l2=0;
-		for(int i=0;i<N;i++) {
-			int num = Integer.parseInt(br.readLine());
-			if(l1==l2) { // 같으면 무조건 앞에 넣어야함 - 그래야 힙1의 배출이 중간값.
-				pq1.offer(num);
-				l1++;
-			} else {
-				pq2.offer(num);
-				l2++;
-			}
-			
-			if(l1!=0 && l2!=0) {
-				if(pq1.peek()>pq2.peek()) {
-					int tmp = pq1.poll();
-					pq1.offer(pq2.poll());
-					pq2.offer(tmp);
-				}
-			}
-			
-			sb.append(pq1.peek()).append("\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> right = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int middle = n;
+        sb.append(middle).append("\n");
+
+        for(int i = 1; i < N ; i++) {
+            n = Integer.parseInt(br.readLine());
+            if(n >= middle) right.add(n);
+            else left.add(n);
+
+            if(i%2==0 && right.size() > left.size()) {
+                left.add(middle);
+                middle = right.poll();
+            }
+            else if(i%2==1 && right.size() -1 > left.size()){
+                left.add(middle);
+                middle = right.poll();
+            }
+            else if(left.size() > right.size()) {
+                right.add(middle);
+                middle = left.poll();
+            }
+
+            sb.append(middle).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
