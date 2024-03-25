643861d7b500569d13b15be8226030fe99060263
정현호 <103182268+Heiness@users.noreply.github.com>
2024-03-25 21:29:36 +0900

    [Gold III] Title: 오등큰수, Time: 1184 ms, Memory: 171372 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.py
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -1,16 +1,13 @@
+import sys; input = sys.stdin.readline
 N = int(input())
-seq = list(map(int, input().split()))
+ans = [-1] * N
+nums = list(map(int,input().split()))
+counter = [0] * 1000001
+for i in nums: counter[i]+=1
+stack = [0]
 
-count = [0] * 1000001
-stack = []
-res = [-1] * N
-for num in seq:
-    count[num] += 1
-
-for i in range(N):
-    while stack and count[seq[stack[-1]]] < count[seq[i]]:
-
-        res[stack.pop()] = seq[i]
+for i in range(1,N):
+    while stack and counter[nums[stack[-1]]] < counter[nums[i]]:
+        ans[stack.pop()] = nums[i]
     stack.append(i)
-
-print(" ".join(map(str,res)))
+print(" ".join(map(str,ans)))
